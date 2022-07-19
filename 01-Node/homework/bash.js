443a3cbba74c6f3f01a9096a95f6913f4f0177a0
AlexT-Dev <atacolmx@gmail.com>
2022-07-19 18:57:41 -0500

    Homework 01-node terminada

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -7,18 +7,36 @@
 //   process.stdout.write('\nprompt > ');
 // });
 
+const { pwd } = require('./commands');
 const commands = require('./commands');
+const cmd = 'pwd';
+
+//commands[cmd](args, done) // la función dentro de la propiedad pwd
+
+//dunció para eliminar el código repetido de las funciones
+
+const done = function (output){ //output es lo que va a imprimir al final de cada comando
+  process.stdout.write(output);
+  process.stdout.write('\nPrompt > ')
+
+}
 
 // Output un prompt
-process.stdout.write('prompt > ');
+process.stdout.write('\n prompt > ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remueve la nueva línea
-  if(cmd === 'date') {
-    process.stdout.write(Date());  
+  var args = data.toString().trim().split(' '); //Quita el primer valo de la cadena Ejemplo: echo hola --> echo
+  var cmd =  args.shift();   //data.toString().trim(); // remueve la nueva línea
+  
+  if(commands.hasOwnProperty(cmd)) {
+    commands[cmd](args,done);
+    //process.stdout.write(Date());  
+  } else {
+    process.stdout.write('Comando invalido.');
+    process.stdout.write('\nprompt > ');
   }
-  if(cmd === 'pwd') {
-    process.stdout.write(pwd());
-  }
-  process.stdout.write('\nprompt > ');
+  // if(cmd === 'pwd') {
+  //   //process.stdout.write(process.cwd());
+  // }
+  //process.stdout.write('\nprompt > ');
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,61 @@
 
-
+const fs = require('fs');
+const request = require('request');
 module.exports = {
+    pwd: function(args, done){done(process.cwd())},
+    date: function(args, done){done(Date())}, 
+    ls: function(args, done){
+        
+                fs.readdir('.', function(err, files) {
+                    if (err) throw err;
+                    var out = '';
+                    files.forEach(function(file) {
+                    //done(file.toString());
+                    out = out + file +'\n'
+                    })
+                    //process.stdout.write("prompt > ");
+                    done(out);
+                });
+    },
+    echo: function(mensaje,done) {
+        done(mensaje.join(" "));
+    },
 
+    cat: function(file, done){
+              fs.readFile(file[0], 'utf8', function(err,data){
+                if(err) throw err;
+                done(data)
+                //process.stdout.write('\n prompt > ')
+              })
+    },
+
+    head: function(file,done){
+        fs.readFile(file[0], 'utf8', function(err,data){
+          if(err) throw err;
+          const lines = data.split('\n').slice(0,9).join('\n');  //Se le quitan al archivo los saltos de linea y los junta las
+                                                                 // primeras 10 líneas
+          done(lines)                                                       
+          //process.stdout.write('\n prompt > ')
+        })
+     },
+    
+
+     tail: function(file, done){
+        fs.readFile(file[0], 'utf8', function(err,data){
+          if(err) throw err;
+          const lines = data.split('\n').slice(-10).join('\n');  //Se le quitan al archivo los saltos de linea y los junta las
+                                                                 // primeras 10 líneas
+          done(lines)                                                       
+          //process.stdout.write('\n prompt > ')
+        })
+     },
+
+     curl: function(url,done){
+        request(url[0], function(err, response, body){
+            if(err) throw err;
+            done(body)                                                       
+            //process.stdout.write('\n prompt > ')
+        })
+     },
 }
+
